4ede3cf6db4caa370a6a87e532983163029251ee
Werner <inqast@gmail.com>
2021-12-01 00:33:02 +0300

    [fix] omniauth fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create  do |user|
       user.email = provider_data.info.email
       user.password = Devise.friendly_token[0, 20]
+      user.provider = provider_data.provider
+      user.uid = provider_data.uid
     end
   end
 end
